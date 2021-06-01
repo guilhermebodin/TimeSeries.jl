17ec40b6f11178bcfbc4f91648d78aa2b3769979
Iblis Lin <iblis@hs.ntnu.edu.tw>
2021-06-01 19:29:08 +0800

    test cases for iterators

---
+++ test/timeaxis/timegrid.jl
@@ -0,0 +1,34 @@
+using Base.Iterators
+using Dates
+using Test
+
+using TimeSeries.TimeAxis
+
+
+@testset "TimeGrid" begin
+
+
+@testset "iterator" begin
+    @testset "fininte iterator" begin
+        tg = TimeGrid(DateTime(2021, 1, 1), Minute(15), 3)
+        @test [i for i ∈ tg] == [
+            DateTime(2021, 1, 1, 0,  0),
+            DateTime(2021, 1, 1, 0, 15),
+            DateTime(2021, 1, 1, 0 ,30),
+        ]
+    end
+
+    @testset "infinite iterator" begin
+        tg = TimeGrid(DateTime(2021, 1, 1), Minute(15))
+        @test [i for i ∈ take(tg, 3)] == [
+            DateTime(2021, 1, 1, 0,  0),
+            DateTime(2021, 1, 1, 0, 15),
+            DateTime(2021, 1, 1, 0 ,30),
+        ]
+        @test length([i for i ∈ take(tg, 4202)]) == 4202
+    end
+
+end  # @testset "iterator
+
+
+end  # @testset "TimeGrid"
