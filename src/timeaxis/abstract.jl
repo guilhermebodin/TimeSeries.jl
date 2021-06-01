9b81973545162095cb29cbd0b7df8a5303a74031
Iblis Lin <iblis@hs.ntnu.edu.tw>
2021-06-01 20:32:40 +0800

    add default printing fallback for AbstractTimeAxis

--- src/timeaxis/abstract.jl
+++ src/timeaxis/abstract.jl
@@ -1,1 +1,9 @@
 abstract type AbstractTimeAxis{T} <: AbstractVector{T} end
+
+
+###############################################################################
+#  Printing
+###############################################################################
+
+Base.summary(io::IO, ata::AbstractTimeAxis) = print(io, typeof(ata))
+Base.show(io::IO, ::MIME{Symbol("text/plain")}, ata::AbstractTimeAxis) = summary(io, ata)
