b185f2aa11706dddfb378b04ed9ce3e4e8dda1cc
Iblis Lin <iblis@hs.ntnu.edu.tw>
2021-06-01 23:22:52 +0800

    issorted

--- src/timeaxis/abstract.jl
+++ src/timeaxis/abstract.jl
@@ -7,9 +7,18 @@ abstract type AbstractTimeAxis{T} <: AbstractVector{T} end
 
 Base.axes(ata::AbstractTimeAxis) = (Base.OneTo(length(ata)),)
 
+
 ###############################################################################
 #  Printing
 ###############################################################################
 
 Base.summary(io::IO, ata::AbstractTimeAxis) = print(io, typeof(ata))
 Base.show(io::IO, ::MIME{Symbol("text/plain")}, ata::AbstractTimeAxis) = summary(io, ata)
+
+
+###############################################################################
+#  Order-related
+###############################################################################
+# https://docs.julialang.org/en/v1/base/sort/#Order-Related-Functions
+
+Base.issorted(ata::AbstractTimeAxis; rev::Bool = false) = !rev
